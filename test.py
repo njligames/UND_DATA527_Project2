dce9f29a7611d150cefd53184434f3d8c7d43fd1
James Folk <jamesfolk1@gmail.com>
2024-05-11 00:31:23 -0400

    trained the final model

--- test.py
+++ test.py
@@ -3,6 +3,13 @@
 import unittest
 
 import csv
+import numpy as np
+
+from NeuralNetwork import NeuralNetwork
+import os.path
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
 def loadRValues(path):
     r_values = None
@@ -107,12 +114,18 @@ def loadFile(fields_in, fields_out):
         item = []
         for field in field_in:
             item.append(field_in_dict[field][i])
-        X.append(item)
+        if len(item) == 1:
+            X.append(item[0])
+        else:
+            X.append(item)
 
         item = []
         for field in field_out:
             item.append(field_out_dict[field][i])
-        Y.append(item)
+        if len(item) == 1:
+            Y.append(item[0])
+        else:
+            Y.append(item)
 
         i = i + 1
 
@@ -140,16 +153,115 @@ def loadFile(fields_in, fields_out):
         mydict.append(item)
     writeFile(filename + ".Y.csv", field_out, mydict)
 
-    return X, Y
+    ind = np.array(X)
+    dep = np.array([Y]).T
+    return ind, dep, X, Y
+
+def calculate_r_squared(y_true, y_pred):
+    if len(y_true) != len(y_pred):
+        raise ValueError("Length of dependant values and predicted values must be the same.")
+
+    # Calculate the mean of the true values
+    mean_y_true = sum(y_true) / len(y_true)
+
+    # Calculate the total sum of squares (TSS) without using sum
+    tss = 0
+    for y in y_true:
+        tss += (y - mean_y_true) ** 2
+
+    # Calculate the residual sum of squares (RSS) without using sum
+    rss = 0
+    for true_val, pred_val in zip(y_true, y_pred):
+        rss += (true_val - pred_val) ** 2
+
+    # Calculate R-squared
+    r_squared = 1 - (rss / tss)
+
+    return r_squared
+
+def kerasModel(inX, inY):
+    X = tf.constant(inX, dtype=tf.float32)
+    Y = tf.constant(inY, dtype=tf.float32)
+
+    # Create a new Sequential Model
+    model = keras.Sequential()
+
+    model.add(layers.Dense(
+        40, # Amount of Neurons
+        input_dim=3, # Define an input dimension because this is the first layer
+        activation='relu' # Use relu activation function because all inputs are positive
+    ))
+    model.add(layers.Dense(
+        40, # Amount of Neurons. We want one output
+        activation='relu' # Use sigmoid because we want to output a binary classification
+    ))
+    model.add(layers.Dense(
+        40, # Amount of Neurons. We want one output
+        activation='relu' # Use sigmoid because we want to output a binary classification
+    ))
+    model.add(layers.Dense(
+        40, # Amount of Neurons. We want one output
+        activation='relu' # Use sigmoid because we want to output a binary classification
+    ))
+    model.add(layers.Dense(
+        40, # Amount of Neurons. We want one output
+        activation='relu' # Use sigmoid because we want to output a binary classification
+    ))
+
+    model.add(layers.Dense(
+        1, # Amount of Neurons. We want one output
+        activation='linear' # Use sigmoid because we want to output a binary classification
+    ))
+    model.compile(
+        loss='mean_squared_error', # The loss function that is being minimized
+        optimizer='adam', # Our optimization function
+        metrics=['accuracy'] # Metrics are different values that you want the model to track while training
+    )
+
+    model.fit(
+        X, # Input training data
+        Y, # Output training data
+        batch_size=3,
+        epochs=500, # Amount of iterations we want to train for
+        verbose=2 # Amount of detail you want shown in terminal while training
+    )
+    model.save("mymodel.keras")
+
+    # inputTens = tf.constant([X[0]], dtype=tf.float32)
+    # val = model.predict(inputTens, verbose=1)[0][0]
+    # print(val)
 
 class TestCalculations(unittest.TestCase):
 
     def test_load(self):
         fields_in = ["altitude", "indicated_airspeed", "roll"]
         fields_out = ["pitch"]
-        X, Y = loadFile(fields_in, fields_out)
-        print(X)
-        print(Y)
+        ind, dep, X, Y = loadFile(fields_in, fields_out)
+
+        kerasModel(X, Y)
+
+        model = tf.keras.models.load_model("mymodel.keras")
+
+        predictions = []
+        for i in range(len(X)):
+            inputTens = tf.constant([X[i]], dtype=tf.float32)
+            prediction = model.predict(inputTens, verbose=2)[0][0]
+            predictions.append(prediction)
+
+        r_squared = calculate_r_squared(Y, predictions)
+        print(f"R Squared: {r_squared}")
+
+
+
+
+
+
+        # truth_table = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+        # expected_output = np.array([[0, 1, 1, 0]]).T
+        # nn = NeuralNetwork(truth_table, expected_output)
+
+        # # Train the network
+        # nn.train(epoch=500000)
 
 
 if __name__ == '__main__':
